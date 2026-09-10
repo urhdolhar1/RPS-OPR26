8e91e43178d259f7361a4a8f862b93c69ca85b79
urhdolhar1 <urhdolhar1@gmail.com>
2026-09-10 06:51:28 +0000

    gg

--- vaje/vaja1.py
+++ vaje/vaja1.py
@@ -1,1 +1,11 @@
-print("Hello World")
+# uvod v API
+import requests # pip install requests
+
+base_url = "https://api.chucknorris.io/jokes/random"
+
+call = requests.get(base_url)
+# print(call.text) preverimo vsebino klica
+
+callJSON = call.json()
+#print(type(callJSON))
+print(callJSON["value"])
