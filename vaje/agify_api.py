fc6d7f2eb8b4bfe4037c7874484727201f0d5d4c
urhdolhar1 <urhdolhar1@gmail.com>
2026-09-23 07:36:19 +0000

    gg

--- vaje/agify_api.py
+++ vaje/agify_api.py
@@ -1,5 +1,8 @@
+import requests
 imena= ["Bine", "Luka"]
-url= "https://api.agify.io?name={imena}"
+url= f"https://api.agify.io?name={imena}"
 
+call = requests.get(url)
+response = call.json()
+#ugotovi katero ime je najstarejše
 for i in imena:
-    
